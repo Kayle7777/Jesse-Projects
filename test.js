0c50e58aa64679570214ac500441ec445d8e10a4
Kayle7777 <jessewebb2@gmail.com>
2017-09-08 16:53:12 -0500

    Typo fix

--- test.js
+++ test.js
@@ -1,50 +1,8 @@
-var userInput = 'rock'
+var hungerLevel = 5
 
-function computerChoice() {
-  var randomNumber = Math.floor(Math.random() * 3)
-  switch(randomNumber) {
-    case 0:
-    return 'rock'
-    case 1:
-    return 'paper'
-    case 2:
-    return 'scissors'
-    break;
-  }
+if (hungerLevel > 7) {
+  console.log('Time to eat!')
 }
-
-var computerChoice = computerChoice()
-
-function determineWinner(userInput, computerChoice) {
-  
-  if(userInput === computerChoice) {
-    return console.log('The game is a tie!');
-  }
-
-  if(userInput === 'rock') {
-    if(computerChoice === 'scissors') {
-      console.log('You win! Your rock crushes scissors!');
-    }
-    else {console.log('You lose! Your rock was covered by paper!');}
-  }
-  
-  if(userInput === 'paper') {
-    if(computerChoice === 'rock') {
-      console.log('You win! Your paper covers rock!');
-    }
-    else {console.log('You lose! Your paper was cut by scissors!');}
-  }
-  if(userInput === 'scissors') {
-    if(computerChoice === 'paper') {
-      console.log('You win! Your scissors cuts paper!');
-    }
-    else {console.log('You lose! Your scissors was crushed by rock!');}
-    }
-  
-  }
-
-function playGame() {
-  console.log(determineWinner());
+else {
+  console.log('Let\'s eat later.')
 }
-
-playGame()
