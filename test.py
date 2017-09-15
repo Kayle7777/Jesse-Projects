0198deaee2b2bf59e1bc3ca5b0c6b6040e024d04
Kayle7777 <jessewebb2@gmail.com>
2017-09-14 21:05:06 -0500

    --

--- test.py
+++ test.py
@@ -1,29 +1,4 @@
-from Tkinter import *
+import sys, urllib
 
-class Application(Frame):
-    def say_hi(self):
-        print "hi there, everyone!"
 
-    def createWidgets(self):
-        self.QUIT = Button(self)
-        self.QUIT["text"] = "QUIT"
-        self.QUIT["fg"]   = "red"
-        self.QUIT["command"] =  self.quit
-
-        self.QUIT.pack({"side": "left"})
-
-        self.hi_there = Button(self)
-        self.hi_there["text"] = "Hello",
-        self.hi_there["command"] = self.say_hi
-
-        self.hi_there.pack({"side": "left"})
-
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.pack()
-        self.createWidgets()
-
-root = Tk()
-app = Application(master=root)[]
-app.mainloop()
-root.destroy()
+urlw = urllib.urlretrieve(sys.argv[1], "c:/users/jesse/desktop/testpics/ " + sys.argv[2])
