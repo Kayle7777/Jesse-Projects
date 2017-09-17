730472d0fea9ab40b609697d3052156470c6cd70
Kayle7777 <jessewebb2@gmail.com>
2017-09-16 20:40:49 -0500

    Click setup

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-import sys, urllib
+import click
 
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+def hello(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo('Hello %s!' % name)
 
-urlw = urllib.urlretrieve(sys.argv[1], "c:/users/jesse/desktop/testpics/ " + sys.argv[2])
+if __name__ == '__main__':
+    hello()
