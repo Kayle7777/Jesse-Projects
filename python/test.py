75679a55299e94012921042f27dc105dd862cc87
Kayle7777 <jessewebb2@gmail.com>
2017-10-07 13:14:02 -0500

    --

--- python/test.py
+++ python/test.py
@@ -1,13 +1,41 @@
-import click
+from tkinter import *
+import tkinter.messagebox
 
-@click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
+def beenClicked():
+    radioValue = relStatus.get()
+    tkinter.messagebox.showinfo("You clicked", radioValue)
+    return
 
-if __name__ == '__main__':
-    hello()
+def changeLabel():
+    name = "Thanks for the click " + yourName.get()
+    labelText.set(name)
+    yourName.delete(0, END)
+    yourName.insert(0, "My name is Jesse")
+    return
+
+app = Tk()
+app.title("GUI Example")
+app.geometry('450x300')
+
+labelText = StringVar()
+labelText.set("Click button")
+label1 = Label(app, textvariable=labelText, height=4)
+label1.pack()
+
+checkBoxVal = IntVar()
+checkBox1 = Checkbutton(app, variable=checkBoxVal, text="Happy?")
+checkBox1.pack()
+
+custName = StringVar(None)
+yourName = Entry(app, textvariable=custName)
+yourName.pack()
+
+relStatus = StringVar()
+relStatus.set(None)
+radio1 = Radiobutton(app, text="Single", value="Single", variable = relStatus, command=beenClicked).pack()
+radio1 = Radiobutton(app, text="Married", value="Married", variable = relStatus, command=beenClicked).pack()
+
+button1 = Button(app, text="Click Here", width=20,command=changeLabel)
+button1.pack(side='bottom',padx=15,pady=15)
+
+app.mainloop()
