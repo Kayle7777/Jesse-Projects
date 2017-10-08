18fbe6a54d72bc3ee19a87422ffde58297018f92
Kayle7777 <jessewebb2@gmail.com>
2017-10-08 12:09:32 -0500

    --

---
+++ python/ex26.py
@@ -0,0 +1,10 @@
+# come back to this when you have wifi
+
+# 33 minutes left, 191 miles to destination
+
+minutes = 33.000
+miles = 191.000
+
+calc = minutes / miles
+
+print "Calculation is %d" % calc
