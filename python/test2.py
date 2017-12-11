30588527443e0e534d9f75e66ab31f12b8818acd
Kayle7777 <jessewebb2@gmail.com>
2017-12-11 12:36:52 -0600

    Working on stuff

--- python/test2.py
+++ python/test2.py
@@ -2,14 +2,23 @@ from tkinter import *
 
 gridloc = []
 
-for x in range(0,25):
-    for y in range(0,25):
+for x in range(0,5):
+    for y in range(0,5):
         gridloc.append((x,y))
 
 root = Tk()
 root.grid()
 
 for b in gridloc:
-    Button(root, text="O").grid(row=b[0],column=b[1])
+    Button(root, text="O",).grid(row=b[0],column=b[1])
+
+blista = []
+for a in enumerate(gridloc):
+    blista.append(a)
+buttonList = [i[0] for i in blista]
+print(buttonList)
+
+def callback:
+    for x in ButtonList
 
 root.mainloop()
