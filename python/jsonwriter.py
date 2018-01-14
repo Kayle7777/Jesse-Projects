7dfffc880ec1f3f8c82fee81672d2f15aeb1021d
Kayle7777 <jessewebb2@gmail.com>
2018-01-14 10:34:32 -0600

    Notation

--- python/jsonwriter.py
+++ python/jsonwriter.py
@@ -3,6 +3,8 @@
 import json
 import codecs
 
+# Returns all entities in a subset section of a json file. For example, every branch on the tree, but no sub branches on the ones returned
+# Example usage: print(jsonwriter('C:\\classes.json','class','name'))
 def jsonopener(file, d, subd):
     list = []
     with codecs.open(file, 'r', 'utf-8-sig') as data_file:
