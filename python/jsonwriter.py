c67a9f5e4b0c80023661a82c0ebc2cffd1ebfb64
Kayle7777 <jessewebb2@gmail.com>
2018-01-13 18:22:53 -0600

    WHY WON'T IT WORK

--- python/jsonwriter.py
+++ python/jsonwriter.py
@@ -1,14 +1,6 @@
 import json
 
-data = {'ObjectInterpolator': 1629,  'PointInterpolator': 1675, 'RectangleInterpolator': 2042}
-
-d = {"class":"name",
-     "children":[{'name':k,"size":v} for k,v in data.items()]}
-
-j = json.dumps(d, indent=4)
-
-f = open('sample.json', 'w')
-
-f.write(j)
-
-f.close()
+with open('C:\\classes.json') as data_file:
+    data = json.load(data_file)
+    for p in data["class"]:
+        print('Name: ' + p['name'])
--- python/urlopener.py
+++
@@ -1,6 +0,0 @@
-from urllib.request import urlopen
-import json
-u = urlopen('https://5etools.com/classes.html')
-resp = json.loads(u.read())
-from pprint import pprint
-pprint(resp)
