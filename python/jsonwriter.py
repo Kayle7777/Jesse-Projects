f29169c38e9a9c61f5317cecd9268a1a71a77dda
Kayle7777 <jessewebb2@gmail.com>
2018-01-14 10:14:25 -0600

    By jove I've got it

--- python/jsonwriter.py
+++ python/jsonwriter.py
@@ -1,6 +1,12 @@
-import json
+#!/bin/python3
 
-with open('C:\\classes.json') as data_file:
-    data = json.load(data_file)
-    for p in data["class"]:
-        print('Name: ' + p['name'])
+import json
+import codecs
+
+def jsonopener(file, d, subd):
+    list = []
+    with codecs.open(file, 'r', 'utf-8-sig') as data_file:
+        data = json.load(data_file)
+        for x in data[d]:
+            list.append(x[subd])
+    return list
