468d2896a65bdf81d43cd893af7395a38ee66ef5
Kayle7777 <jessewebb2@gmail.com>
2018-01-14 13:22:37 -0600

    Modules are Cool

--- python/randomcharsheet.py
+++ python/randomcharsheet.py
@@ -1,4 +1,8 @@
 import random, json
+import jsonwriter as jw
 
 def randomizer(min, max):
     return random.randint(min,max)
+print(randomizer(1,6))
+
+print(jw.jsonwriter('C:\\classes.json','class','name'))
