61095920379fd288415e6e1a96b2e32b60de7e7d
Kayle7777 <jessewebb2@gmail.com>
2018-01-22 15:17:55 -0600

    Derrick hjalped

--- python/randomcharsheet.py
+++ python/randomcharsheet.py
@@ -26,11 +26,11 @@ def rolljson(thing,min,max):
 classresult = rolljson("classes",1,11)
 raceresult = rolljson("races",1,14)
 
-# print(statslist[0])
-# if str(classresult) == "Mountain Dwarf" or "Half-Orc" or "Dragonborn":
-#     statslist[0][1] = statslist[0][1] + 2
-# elif str(classresult) == "Human":
-#     statslist[0][1] = statslist[0][1] + 1
+print(statslist[0])
+if str(classresult) == "Mountain Dwarf" or str(classresult) == "Half-Orc" or str(classresult) == "Dragonborn":
+    statslist[0][1] = statslist[0][1] + 2
+elif str(classresult) == "Human":
+    statslist[0][1] = statslist[0][1] + 1
 
 print("You are a " + str(raceresult) + " " + str(classresult) + "!" + " " + "Your stats are:\n")
 print(statslist)
