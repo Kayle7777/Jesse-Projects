108da515af1422477fa9a2fdae3e4e613d5bf83c
Kayle7777 <jessewebb2@gmail.com>
2018-02-09 18:14:07 -0600

    objectpath test stuff

--- python/test2.py
+++ python/test2.py
@@ -1,24 +1,11 @@
-from tkinter import *
+import random, json, codecs
+from collections import defaultdict
+from itertools import chain
+from pprint import *
+import objectpath
 
-gridloc = []
-
-for x in range(0,5):
-    for y in range(0,5):
-        gridloc.append((x,y))
-
-root = Tk()
-root.grid()
-
-for b in gridloc:
-    Button(root, text="O",).grid(row=b[0],column=b[1])
-
-blista = []
-for a in enumerate(gridloc):
-    blista.append(a)
-buttonList = [i[0] for i in blista]
-print(buttonList)
-
-#def callback:
-#    for x in ButtonList
-
-root.mainloop()
+file = "data/background.json"
+with codecs.open(file, "r", "utf-8-sig") as data_file:
+    f = json.load(data_file)
+    data = objectpath.Tree(f)
+    print(data.execute("$.background[0].entries[0:1]"))
