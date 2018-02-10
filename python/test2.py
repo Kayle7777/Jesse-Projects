e12c9e92659ae62ab588bfbac43b17bc9621f8eb
Kayle7777 <jessewebb2@gmail.com>
2018-02-09 18:27:17 -0600

    progress

--- python/test2.py
+++ python/test2.py
@@ -8,4 +8,5 @@ file = "data/background.json"
 with codecs.open(file, "r", "utf-8-sig") as data_file:
     f = json.load(data_file)
     data = objectpath.Tree(f)
-    print(data.execute("$.background[0].entries[0:1]"))
+    fnl = [f["background"][n]["name"] for n in range(len(f["background"]))]
+    print(list(data.execute("$..source")))
