2764fff1a670fe6150b666f7798bd3b4ab33c38a
Kayle7777 <jessewebb2@gmail.com>
2018-02-11 15:55:32 -0600

    Learning more objectpath

--- python/test2.py
+++ python/test2.py
@@ -9,4 +9,5 @@ with codecs.open(file, "r", "utf-8-sig") as data_file:
     f = json.load(data_file)
     data = objectpath.Tree(f)
     fnl = [f["background"][n]["name"] for n in range(len(f["background"]))]
-    print(list(data.execute("$..source")))
+    test = "name"
+    print(list(data.execute("$.background[@.source is PHB].("+test+", source)")))
