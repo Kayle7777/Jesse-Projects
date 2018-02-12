d93141aaa4cd178385ba0bdd22623ee6160d92ac
Kayle7777 <jessewebb2@gmail.com>
2018-02-12 16:02:03 -0600

    sync

--- python/test.py
+++ python/test.py
@@ -21,22 +21,22 @@ def listShuffler(data, count):
     data = [data[x] for x in range(count)]
     return data
 
-def inList(list1, list2):
-    inlist = [x for x in list2 if x in list1]
-    return inlist
-
-def getProfs(t, pick):
+def getStuff(t, pick):
     file = "data/" + t + ".json"
     with codecs.open(file, "r", "utf-8-sig") as data_file:
         data = json.load(data_file)
         profs = []
+        entries = []
         fnl = [data[t][n]["name"] for n in range(len(data[t]))]
         if t == "race":
             raceSkillData = data["race"][fnl.index(pick)]
+            entries.append(raceSkillData["entries"])
             if "proficiency" in raceSkillData:
                 profs.append(raceSkillData["proficiency"])
         if t == "class":
-            classSkillData = data["class"][fnl.index(pick)]["startingProficiencies"]["skills"]
+            classStuff = data["class"][fnl.index(pick)]
+            PrettyPrinter().pprint(classStuff)
+            classSkillData = classStuff["startingProficiencies"]["skills"]
             if "choose" in classSkillData:
                 profs.extend(listShuffler(classSkillData["from"], classSkillData["choose"]))
             else:
@@ -73,24 +73,15 @@ def racialStats(t):
         fullstats = {key: sum(values) for (key, values) in dictChain(statslist, racestats).items()}
         return [fullstats, racestats]
 
-def getEntries(t, pick):
-    file = "data/" + t + ".json"
-    with codecs.open(file, "r", "utf-8-sig") as data_file:
-        data = json.load(data_file)
-        fnl = [data[t][n]["name"] for n in range(len(data[t]))]
-        f = data[t][fnl.index(pick)]["entries"]
-        entry = [f[x] for x in range(4, len(f))]
-    return entry
-
 statslist = {x: statsRoller() for x in ["str","dex","con","int","wis","cha"]}
 
 raceStuff = rollJson("race")
 classStuff = rollJson("class")
 backgroundStuff = rollJson("background")
 
-sumProfs = [getProfs("background", backgroundStuff) + getProfs("class", classStuff)]
-if getProfs("race", raceStuff) != []:
-    sumProfs.append(getProfs("race", raceStuff))
+#sumProfs = [getStuff("background", backgroundStuff) + getStuff("class", classStuff)]
+#if getStuff("race", raceStuff) != []:
+#    sumProfs.append(getStuff("race", raceStuff))
 
 featStuff = None
 if raceStuff == "Human (Variant)":
@@ -106,41 +97,23 @@ jsonPush ["Stats"] = {
 jsonPush ["Race"] = {
 "name": raceStuff,
 "statsBonus": racialStats(raceStuff)[1],
-"raceProficiencies": getProfs("race", raceStuff)
+"raceProficiencies": getStuff("race", raceStuff)
 }
 jsonPush ["Class"] = {
 "name": classStuff,
 "subclass": None,
-"classProficiencies": getProfs("class", classStuff)
+"classProficiencies": getStuff("class", classStuff)
 }
 jsonPush ["Background"] = {
 "name": backgroundStuff,
-"backgroundProficiencies": getProfs("background", backgroundStuff)
+"backgroundProficiencies": getStuff("background", backgroundStuff)
 }
 
 if featStuff != None:
     jsonPush ["Feat"] = {
-    "name": featStuff,
-
+    "name": featStuff
     }
 
-PrettyPrinter(indent=2).pprint(jsonPush)
-#EXAMPLE OUTPUT
-"""
-{ 'Background': { 'backgroundProficiencies': ['Athletics, Intimidation'],
-                  'name': 'Soldier'},
-  'Class': { 'classProficiencies': [ 'Nature',
-                                     'Religion',
-                                     'Int Saving Throws',
-                                     'Wis Saving Throws'],
-             'name': 'Druid',
-             'subclass': None},
-  'Race': { 'name': 'Triton',
-            'raceProficiencies': [],
-            'statsBonus': {'cha': 1, 'con': 1, 'str': 1}},
-  'Stats': { 'afterRaceBonus': { 'cha': 14,
-                                 'con': 8,
-                                 'dex': 12,
-                                 'int': 13,
-                                 'str': 14,
-                                 'wis': 11}}}"""
+
+#with open("sheets/" + raceStuff + " " + classStuff + " " + backgroundStuff + ".json", "w") as f:
+#    f.write(json.dumps(jsonPush, indent=2))
--- python/test2.py
+++ python/test2.py
@@ -4,10 +4,24 @@ from itertools import chain
 from pprint import *
 import objectpath
 
-file = "data/background.json"
+"""file = "data/background.json"
 with codecs.open(file, "r", "utf-8-sig") as data_file:
     f = json.load(data_file)
     data = objectpath.Tree(f)
     fnl = [f["background"][n]["name"] for n in range(len(f["background"]))]
     test = "name"
-    PrettyPrinter(indent=2).pprint(list(data.execute("$.background[@.source is PHB].(" + test + ", source, skillProficiencies)")))
+    PrettyPrinter(indent=2).pprint(list(data.execute("$.background[@.source is PHB].(" + test + ", source, skillProficiencies)")))"""
+
+def getEntries(t, pick):
+    file = "data/" + t + ".json"
+    with codecs.open(file, "r", "utf-8-sig") as data_file:
+        data = json.load(data_file)
+        fnl = [data[t][n]["name"] for n in range(len(data[t]))]
+        stuff = data[t][fnl.index(pick)]["entries"]
+#        print(stuff)
+#        tree = objectpath.Tree(stuff)
+#        f = list(tree.execute("$"))
+    return stuff
+
+testvar = getEntries("race", "Human") + getEntries("class", "Ranger") + getEntries("background", "Acolyte")
+print(testvar)
