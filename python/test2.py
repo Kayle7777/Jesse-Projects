12565abd631f807c7e285d81b8f536401f97f700
Kayle7777 <jessewebb2@gmail.com>
2018-02-13 12:44:29 -0600

    MATH

--- python/test2.py
+++ python/test2.py
@@ -1,9 +1,8 @@
-import random, json, codecs
-from collections import defaultdict
-from itertools import chain
+import random
 
 def randomListSum(n, total):
     randomNumbers = sorted(random.sample(range(1, total), n - 1))
-    return [a - b for a, b in zip(randomNumbers + [total], [0] + randomNumbers)]
+    x = [a - b for a, b in zip(randomNumbers + [total], [0] + randomNumbers)]
+    return x
 
 print(randomListSum(5, 100))
