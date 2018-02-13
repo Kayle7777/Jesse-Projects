4b4fa05fe47a6a14adecc43b247f5c9d5a396e52
Kayle7777 <jessewebb2@gmail.com>
2018-02-13 12:35:11 -0600

    Cool Math

--- python/test2.py
+++ python/test2.py
@@ -1,36 +1,9 @@
 import random, json, codecs
 from collections import defaultdict
 from itertools import chain
-#from pprint import *
-#import objectpath
 
-"""file = "data/background.json"
-with codecs.open(file, "r", "utf-8-sig") as data_file:
-    f = json.load(data_file)
-    data = objectpath.Tree(f)
-    fnl = [f["background"][n]["name"] for n in range(len(f["background"]))]
-    test = "name"
-    PrettyPrinter(indent=2).pprint(list(data.execute("$.background[@.source is PHB].(" + test + ", source, skillProficiencies)")))"""
+def randomListSum(n, total):
+    randomNumbers = sorted(random.sample(range(1, total), n - 1))
+    return [a - b for a, b in zip(randomNumbers + [total], [0] + randomNumbers)]
 
-def newJson(t):
-    file = "data/" + t + ".json"
-    with codecs.open(file, "r", "utf-8-sig") as data_file:
-        data = json.load(data_file)
-        fnl = [data[t][n]["name"] for n in range(len(data[t]))]
-        newnl = [data[t][n]["name"] for n in range(len(data[t])) if data[t][n]["source"] == "PHB" or data[t][n]["source"] == "VGM" or data[t][n]["source"] == "XGE"]
-        f = [data[t][fnl.index(n)] for n in newnl]
-#        tree = objectpath.Tree(stuff)
-#        f = list(tree.execute("$"))
-    return f, t.title()
-
-#newJsonRace = newJson("race")
-#newJsonClass = newJson("class")
-#newJsonBackground = newJson("background")
-
-def doTheThing(a, b):
-    with open("data/noUA" + a + ".json", "w") as f:
-        f.write(json.dumps(b, indent=2))
-
-#doTheThing(newJsonRace[1], newJsonRace[0])
-#doTheThing(newJsonClass[1], newJsonClass[0])
-#doTheThing(newJsonBackground[1], newJsonBackground[0])
+print(randomListSum(5, 100))
