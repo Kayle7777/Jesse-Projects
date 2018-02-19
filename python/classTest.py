801c1a4c9588845c3b91eb783455b3142d28c28c
Kayle7777 <jessewebb2@gmail.com>
2018-02-19 16:08:24 -0600

    removing outdated "with" thing

--- python/classTest.py
+++ python/classTest.py
@@ -1,5 +1,6 @@
 import random, json, codecs
 from testFunctions import listShuffler
+from pprint import *
 
 class CharacterSheet:
     "For learning"
@@ -7,39 +8,41 @@ class CharacterSheet:
     def __init__(self, t):
 
         self.file = "data/" + t + ".json"
-        self.filename = t
+        self.t = t
         self.data = json.load(codecs.open(self.file, "r", "utf-8-sig"))
-
-        with codecs.open(self.file, "r", "utf-8-sig") as data_file:
-
-            self.namelist = [self.data[t][n]["name"] for n in range(len(self.data[t])) if self.data[t][n]["source"] == "PHB" or self.data[t][n]["source"] == "XGE" or self.data[t][n]["source"] == "VGM"]
-            rtd = random.randint(0,len(self.namelist)-1)
-            self.name = self.namelist[rtd]
-            self.fullnamelist = [self.data[t][n]["name"] for n in range(len(self.data[t]))]
-            self.number = self.fullnamelist.index(self.name)
-            return
+        self.namelist = [self.data[t][n]["name"] for n in range(len(self.data[t])) if self.data[t][n]["source"] == "PHB" or self.data[t][n]["source"] == "XGE" or self.data[t][n]["source"] == "VGM"]
+        rtd = random.randint(0,len(self.namelist)-1)
+        self.name = self.namelist[rtd]
+        self.index = [self.data[t][n]["name"] for n in range(len(self.data[t]))].index(self.name)
+        return
 
     def getStuff(self):
-        with codecs.open(self.file, "r", "utf-8-sig") as data_file:
-            self.profs = []
-            if self.filename == "race":
-                raceSkillData = self.data["race"][self.fullnamelist.index(self.name)]
-                if "proficiency" in raceSkillData:
-                    self.profs.append(raceSkillData["proficiency"])
-            if self.filename == "class":
-                classSkillData = self.data["class"][self.fullnamelist.index(self.name)]["startingProficiencies"]["skills"]
-                if "choose" in classSkillData:
-                    self.profs.extend(listShuffler(classSkillData["from"], classSkillData["choose"]))
-                else:
-                    self.profs.extend(self.data[self.filename][self.fullnamelist.index(self.name)]["startingProficiencies"])
-                self.profs.extend([self.data["class"][self.fullnamelist.index(self.name)]["proficiency"][x].title() + " Saving Throws" for x in range(2)])
-            if self.filename == "background":
-                self.profs.append(self.data[self.filename][self.fullnamelist.index(self.name)]["skillProficiencies"])
-            return self.profs
+        self.profs = []
+        if self.t == "race":
+            raceSkillData = self.data["race"][self.index]
+            if "proficiency" in raceSkillData:
+                self.profs.append(raceSkillData["proficiency"])
+        if self.t == "class":
+            classSkillData = self.data["class"][self.index]["startingProficiencies"]["skills"]
+            if "choose" in classSkillData:
+                self.profs.extend(listShuffler(classSkillData["from"], classSkillData["choose"]))
+            else:
+                self.profs.extend(self.data[self.t][self.index]["startingProficiencies"])
+            self.profs.extend([self.data["class"][self.index]["proficiency"][x].title() + " Saving Throws" for x in range(2)])
+        if self.t == "background":
+            self.profs.append(self.data[self.t][self.index]["skillProficiencies"])
+
+        del self.data, self.namelist # we are done with the huge dicts "data" and "namelist" for now
+
+        return self.profs
 
 testrace = CharacterSheet("race")
 testclass = CharacterSheet("class")
 testbackground = CharacterSheet("background")
 proficiencies = testrace.getStuff() + testclass.getStuff() + testbackground.getStuff()
-print(testclass.name)
+print(testrace.name + " " + testclass.name + " " + testbackground.name)
 print(proficiencies)
+
+PrettyPrinter(indent=2).pprint(testrace.__dict__)
+PrettyPrinter(indent=2).pprint(testclass.__dict__)
+PrettyPrinter(indent=2).pprint(testbackground.__dict__)
