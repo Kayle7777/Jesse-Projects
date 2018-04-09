77303fdbfc205451feea574272599a1379a70c54
Kayle7777 <jessewebb2@gmail.com>
2018-04-09 10:22:46 -0500

    wxHelloWorld...

--- python/wxTest.py
+++ python/wxTest.py
@@ -1,11 +1,17 @@
-#!/bin/python
-
 import wx
+
+# This is an application object
 
 app = wx.App()
 
+# This is a frame
+
 frame = wx.Frame(None, title="Hello World")
 
+# Show it with this...
+
 frame.Show()
+
+# "Event Main Loop..."
 
 app.MainLoop()
