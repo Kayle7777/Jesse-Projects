53c52d0fc75519866abaf76e90b9fb2289d1453c
Kayle7777 <jessewebb2@gmail.com>
2018-05-24 19:00:37 -0500

    Test

--- javascript/helloworld.js
+++
@@ -1,7 +0,0 @@
-function randInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-let a = ['World','Linus','Osei','Greg','Jesse','David','Daniel','Blake']
-
-console.log('Hello' + " " + a[randInt(a.length)] + "!")
