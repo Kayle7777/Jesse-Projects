d92bd1a6980c766840eee17e418e55152a91225e
Kayle7777 <jessewebb2@gmail.com>
2018-05-24 19:50:40 -0500

    cleanup

--- warriorjs-master/warriorjs/jesse-beginner/Player.js
+++ warriorjs-master/warriorjs/jesse-beginner/Player.js
@@ -1,6 +1,7 @@
 class Player {
   constructor() {
     this.health = 20;
+    this.names = [];
   }
 
   vector(x,y) {
@@ -19,34 +20,21 @@ class Player {
       return "I am injured."
     } else {return "I am not injured."}
   }
-// it seems .think is a way to execute functions about stuff
+// it seems .think is a way to console.log stuff
   playTurn(warrior) {
     let healthStatus = warrior.think(this.isInjured());
-    if (healthStatus = "I am fine") {
-      let x = this.vector(['forward', 'right', 'backward', 'left'], warrior);
+//    if (healthStatus = "I am fine") {
+    let x = this.vector(['forward', 'right', 'backward', 'left'], warrior);
 
-      // try thinking Object.keys(x) to list object properties
-      // x properties getLocation,getUnit,isEmpty,isStairs,isUnit,isWall
-      // x is now a list of objects
-      // warrior.think('I am thinking ' + Object.keys(x[0]))
-      let names = [];
-      for (var i = 0; i < x.length; i++) {names.push(x[i].name)}
-      warrior.think("Now I'm going to think if " + names + "are empty...")
-      for (var i = 0; i < x.length; i++){
-        warrior.think('I am thinking ' + x[i].isEmpty() + ".")
-      }
-      }
+    // try thinking Object.keys(x) to list object properties
+    // x properties getLocation,getUnit,isEmpty,isStairs,isUnit,isWall
+    // x is now a list of objects
+    // warrior.think('I am thinking ' + Object.keys(x[0]))
+
+    for (var i = 0; i < x.length; i++) {this.names.push(x[i].name)}
+    warrior.think("Now I'm going to think if " + this.names + " are empty...")
+    for (var i = 0; i < x.length; i++){
+      warrior.think(x[i].name + ' is ' + x[i].isEmpty() + ".")}
+//      }
     }
 }
-
-  // feeler(context) {
-    // var defaults = {
-    //   n: 'north',
-    //   e: 'east',
-    //   s: 'south',
-    //   w: 'west'
-    // };
-    // var context = extend(defaults, context);
-    // for (var i = 0; i < context.length; i++) {
-    //   this.directions.push(Player.warrior.feel(x))
-    // }
